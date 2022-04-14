13a34fd72ec4c323d917f2a631fed64f616731ae
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2022-04-14 18:21:24 +0530

    added orderhistory page

--- client/src/components/OrderHistory.js
+++ client/src/components/OrderHistory.js
@@ -33,21 +33,10 @@ const AddToCart = () => {
                         orderHistory.map((orderItem) => {
                             let items = orderItem.items;
                             items.map((orderNth) => {
-                                let { ID, name, image, price, ratting } = orderNth;
                                 return (
-                                    <div className="row my-2" style={{ background: 'bisque' }} key={ID}>
-                                        <div className="col-4 px-0">
-                                            <div style={{ width: '100px' }}>
-                                                <img src={image} style={{ "borderRadius": "2px 2px 2px 2px", "width": "100%", "height": "100%" }} className="card-img-top" alt="INA" />
-                                            </div>
-                                        </div>
-                                        <div className="col-4 d-flex flex-column justify-content-center smallAddtoCart">
-                                            <p style={{ fontWeight: 'bold' }} className="my-auto">{name}</p>
-                                            <p className="card-text my-auto" style={{ 'fontWeight': 'bolder' }}><i className="fas fa-rupee-sign"></i> {price}</p>
-                                        </div>
-                                        <div className="col-4 d-flex flex-column justify-content-center align-items-center">
-                                            <p style={{ width: '3.5rem', "background": "green", 'textAlign': 'center', "borderRadius": '7px', "color": 'white', 'cursor': 'pointer' }} className="card-text mx-auto col-md-4 my-auto">{ratting}<i style={{ "color": 'white' }} className="bi bi-star-fill"></i></p>
-                                        </div>
+                                    <div id={orderNth.ID}>
+                                        {console.log(orderNth.name)}
+                                        <h1>{orderNth.name}</h1>
                                     </div>
                                 )
                             })
